9cb2e350dddf98238ec6012c34afa45fa59b730c
AustereTriceratops <gatewaylevi@gmail.com>
2026-06-18 21:52:20 -0700

    level 6

--- block_manager.gd
+++ block_manager.gd
@@ -57,7 +57,7 @@ const breaker_tiles_level_2 : Array[Vector2] = [Vector2(3, 0), Vector2(2,0)]
 const breaker_tiles_level_3 : Array[Vector2] = [Vector2(0, 2), Vector2(0, 3)]
 const breaker_tiles_level_4 : Array[Vector2] = [Vector2(0, 0), Vector2(1,0)]
 const breaker_tiles_level_5 : Array[Vector2] = [Vector2(1, 0), Vector2(2,0)]
-const breaker_tiles_level_6 : Array[Vector2] = [Vector2(3, 0), Vector2(2,0)]
+const breaker_tiles_level_6 : Array[Vector2] = [Vector2(2, 3), Vector2(3,3)]
 # locations for wind breaker tiles in each level
 var breaker_tiles_levels = [
 	breaker_tiles_level_0,
